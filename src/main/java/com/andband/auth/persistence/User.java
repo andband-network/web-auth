a65f91d70bcb574f25faa199992551192aae2a57
andband <finnhoulihan@email.com>
2019-05-06 03:56:40 +0100

    removed the need for a hibernate sequence table

--- src/main/java/com/andband/auth/persistence/User.java
+++ src/main/java/com/andband/auth/persistence/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username", nullable = false, unique = true)
